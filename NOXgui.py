e9c974f2c15189c0871a389ead8d23f5f6d92125
RedWave <redwave2111@gmail.com>
2023-08-03 13:42:00 -0600

    canvas con comandos y con info de búsqueda

--- NOXgui.py
+++ NOXgui.py
@@ -17,13 +17,32 @@ from PIL import ImageTk, Image
 main_window = Tk() #Ventana raíz
 main_window.title("NOX Assistant")
 
-main_window.geometry("800x400")#Ancho x alto
+main_window.geometry("800x450")#Ancho x alto
 main_window.resizable(0,0)#Se elimina el resize
 main_window.configure(bg="#0F2027")#fondo
+
+comandos = """
+        Comandos disponibles:
+        - Reproduce ... (canción)
+        - Busca ... (algo)
+        - Abre ... (pag o app)
+        - Alarma ... (hora en 24hrs)
+        - Archivo ... (nombre)
+        - Escribe ... (info a anotar)
+        - Termina para que deje de
+          escuchar
+"""
 
 
 label_title = Label(main_window, text="NOX", bg="#203A43", fg="#000000", font=("Arial", 20,'bold'))
 label_title.pack(pady=10)#vuelve a la label un bloque y lo pone en el centro con padding de 10 px
+
+canvas_comandos = Canvas(bg="#2c3e50", height=170, width=195)
+canvas_comandos.place(x=0,y=0)
+canvas_comandos.create_text(90,80, text = comandos, fill="white", font='Arial 10')
+
+text_info = Text(main_window,bg="#2c3e50",fg="white")
+text_info.place(x=0,y=175,height=275, width=198)
 
 nox_photo = ImageTk.PhotoImage(Image.open(r"C:\Users\zjosh\Desktop\RW\Proyectoss\AsistentePY\NOX\R.jpg"))
 window_photo = Label(main_window, image=nox_photo)
@@ -37,9 +56,9 @@ def change_voice(id):
     engine.setProperty('voice', voices[id].id)
     engine.setProperty('rate', 145)
     if id == 2:
-        talk("Hola; Soy NOX")
+        talk("Hola; Soy Nox")
     else:
-        talk("Hello; Im NOX")
+        talk("Hello; Im Nox")
 
 name = "Nox"
 listener = sr.Recognizer()
@@ -50,7 +69,7 @@ engine.setProperty('voice', voices[2].id)
 engine.setProperty('rate', 145)
 
 
-"""
+
 sites = {
     'google': 'google.com',
     'youtube': 'youtube.com',
@@ -78,13 +97,18 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+def read_and_talk():
+    text = text_info.get("1.0", "end") #Obtiene el texto de principio a fin
+    talk(text)
+def write_text(text_wiki):
+    text_info.insert(INSERT, text_wiki)
 
 def listen():
     rec = ""  # Asignamos un valor predeterminado a la variable rec
     try:
         with sr.Microphone() as source:
 
-            print("Te escucho...")
+            talk("Te escucho...")
             pc = listener.listen(source)
             rec = listener.recognize_google(pc, language="es")
             rec = rec.lower()
@@ -101,7 +125,6 @@ def run_nox():
         rec = listen()
         if 'reproduce' in rec:
             music = rec.replace('reproduce', '')
-            print("Reproducinedo " + music)
             talk("Reproduciendo " + music)
             # Hace el proceso de reproducción en youtube
             pywhatkit.playonyt(music)
@@ -109,8 +132,9 @@ def run_nox():
             search = rec.replace('busca', '')
             wikipedia.set_lang("es")
             wiki = wikipedia.summary(search, 1)
-            print(search + ": "+wiki)
             talk(wiki)
+            write_text(search + ": "+wiki)
+            break #Muestra la info después de que deja de escuchar así que hay que darle de nuevo al botón
         elif 'alarma' in rec:
             num = rec.replace('alarma', '')
             # Esto sirve para que no haya un espacio de más
@@ -168,7 +192,6 @@ def write(f):
     talk("¡Ready!, chek it out")
     sub.Popen("nota.txt", shell=True)
 
-"""
 
 button_voice_mx = Button(main_window, text="Voz México", fg="white", bg="#38ef7d", 
                          font=("Arial",10,"bold"), command=mexico_voice)
@@ -176,9 +199,15 @@ button_voice_mx.place(x=625,y=90, width=100, height=30)
 
 button_voice_us = Button(main_window, text="Voz USA", fg="white", bg="#11998e", 
                          font=("Arial",10,"bold"), command=usa_voice)
-button_voice_mx.place(x=625,y=120, width=100, height=30)
+button_voice_us.place(x=625,y=50, width=100, height=30)
 
+button_listen = Button(main_window, text="Escuchar", fg="white", bg="#23074d", 
+                         font=("Arial",15,"bold"),width=20,height=3 ,command=run_nox)
+button_listen.pack(pady=10)
 
+button_speak = Button(main_window, text="Hablar", fg="white", bg="#23074d", 
+                         font=("Arial",10,"bold"), command=read_and_talk)
+button_speak.place(x=625,y=150, width=100, height=30)
 
 
 main_window.mainloop()#HAce que el código se ejecute solo cuando la ventana esté ejecutándose
